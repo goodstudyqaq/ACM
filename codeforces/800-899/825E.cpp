e446ac24dad2b616e86e98f50cb4321092721e46
shen <goodstudyQAQ@163.com>
2021-05-31 00:37:04 +0800

    [2300] CF825E topu

---
+++ codeforces/800-899/825E.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string)s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() {
+    cerr << endl;
+}
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+const int maxn = 1e5 + 5;
+vector<int> V[maxn];
+int ans[maxn];
+int in[maxn];
+int n, m;
+
+void work() {
+    priority_queue<int> Q;
+    for (int i = 1; i <= n; i++) {
+        if (in[i] == 0) {
+            Q.push(i);
+        }
+    }
+
+    int val = n;
+    while (!Q.empty()) {
+        int x = Q.top();
+        Q.pop();
+        ans[x] = val;
+        val--;
+
+        for (auto v : V[x]) {
+            in[v]--;
+            if (in[v] == 0) {
+                Q.push(v);
+            }
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    int u, v;
+    for (int i = 1; i <= m; i++) {
+        scanf("%d %d", &u, &v);
+        V[v].push_back(u);
+        in[u]++;
+    }
+    work();
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", ans[i]);
+    }
+    puts("");
+    return 0;
+}
