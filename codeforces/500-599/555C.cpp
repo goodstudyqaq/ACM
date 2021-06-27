4ac58a7f52af7c9624b3119b5b12d1f4d0ec0cb7
goodstudy <goodstudyQAQ@163.com>
2021-06-27 22:29:36 +0800

    [2200] CF555C STL map 分类讨论 线段树

---
+++ codeforces/500-599/555C.cpp
@@ -0,0 +1,209 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+const int maxn = 4e5 + 5;
+
+#define lson l, m, rt << 1
+#define rson m + 1, r, rt << 1 | 1
+
+struct SegTree {
+	int mx[maxn << 2];
+	int lazy[maxn << 2];
+
+	void push_up(int rt) {
+		mx[rt] = max(mx[rt << 1], mx[rt << 1 | 1]);
+	}
+
+	void push_down(int rt) {
+		if (lazy[rt]) {
+			mx[rt << 1] = max(mx[rt << 1], lazy[rt]);
+			mx[rt << 1 | 1] = max(mx[rt << 1 | 1], lazy[rt]);
+			lazy[rt << 1] = max(lazy[rt << 1], lazy[rt]);
+			lazy[rt << 1 | 1] = max(lazy[rt << 1 | 1], lazy[rt]);
+			lazy[rt] = 0;
+		}
+	}
+
+	void build(int l, int r, int rt) {
+		mx[rt] = 1;
+		lazy[rt] = 0;
+		if (l == r) {
+			return;
+		}
+		int m = l + r >> 1;
+		build(lson);
+		build(rson);
+		push_up(rt);
+	}
+
+	void update(int L, int R, int val, int l, int r, int rt) {
+		if (L <= l && r <= R) {
+			mx[rt] = max(mx[rt], val);
+			lazy[rt] = max(lazy[rt], val);
+			return;
+		}
+		push_down(rt);
+		int m = l + r >> 1;
+		if (L <= m) update(L, R, val, lson);
+		if (R > m) update(L, R, val, rson);
+		push_up(rt);
+	}
+
+	int query(int L, int l, int r, int rt) {
+		if (l == r) {
+			return mx[rt];
+		}
+		push_down(rt);
+		int m = l + r >> 1;
+		if (L <= m)
+			return query(L, lson);
+		else
+			return query(L, rson);
+	}
+} s1, s2;
+typedef pair< int, int > pii;
+typedef pair< pii, char > piic;
+piic querys[maxn];
+int xx[maxn], xcnt;
+
+int get_idx(int val) {
+	return lower_bound(xx, xx + xcnt, val) - xx + 1;
+}
+bool vis1[maxn], vis2[maxn];
+
+int main() {
+	freopen("data1.in", "r", stdin);
+	int n, q;
+	scanf("%d %d", &n, &q);
+	int x, y;
+	char c;
+	for (int i = 1; i <= q; i++) {
+		scanf("%d %d %c", &x, &y, &c);
+		querys[i] = { { x, y }, c };
+		xx[xcnt++] = x;
+		xx[xcnt++] = y;
+	}
+	xx[xcnt++] = 0;
+	sort(xx, xx + xcnt);
+	xcnt = unique(xx, xx + xcnt) - xx;
+
+	s1.build(1, xcnt, 1);
+	s2.build(1, xcnt, 1);
+
+	for (int i = 1; i <= q; i++) {
+		x = querys[i].first.first, y = querys[i].first.second;
+		c = querys[i].second;
+		int xidx = get_idx(x), yidx = get_idx(y);
+		if (c == 'U') {
+			if (vis1[xidx]) {
+				puts("0");
+				continue;
+			}
+			vis1[xidx] = 1;
+			int val_idx = s1.query(xidx, 1, xcnt, 1);
+			printf("%d\n", y - xx[val_idx - 1]);
+			s2.update(val_idx + 1, yidx, xidx, 1, xcnt, 1);
+		} else {
+			if (vis2[yidx]) {
+				puts("0");
+				continue;
+			}
+			vis2[yidx] = 1;
+			int val_idx = s2.query(yidx, 1, xcnt, 1);
+			printf("%d\n", x - xx[val_idx - 1]);
+			s1.update(val_idx + 1, xidx, yidx, 1, xcnt, 1);
+		}
+	}
+	return 0;
+}
