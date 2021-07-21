5ff7d2b7dae946a20233d40bd4f00e4e925e6788
goodstudy <goodstudyQAQ@163.com>
2021-07-21 09:40:05 +0800

    [2100] CF319C 斜率优化

---
+++ codeforces/300-399/319C.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+vector< long long > a, b;
+vector< long long > dp;
+
+long long gety(int idx) {
+	return dp[idx];
+}
+
+int getx(int idx) {
+	return -b[idx];
+}
+
+double getk(int idx) {
+	return a[idx];
+}
+
+double getslope(int j, int k) {
+	return 1.0 * (gety(k) - gety(j)) / (getx(k) - getx(j));
+}
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	a.resize(n + 1);
+	b.resize(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 1; i <= n; i++) {
+		cin >> b[i];
+	}
+
+	vector< int > Q(n + 1);
+	dp.resize(n + 1);
+	int first = 0, tail = 0;
+	Q[tail] = 1;
+	dp[1] = 0;
+
+	for (int i = 2; i <= n; i++) {
+		while (first < tail && getslope(Q[first], Q[first + 1]) <= getk(i)) {
+			first++;
+		}
+		dp[i] = dp[Q[first]] + 1LL * a[i] * b[Q[first]];
+
+		while (first < tail && getslope(Q[tail], i) <= getslope(Q[tail - 1], Q[tail])) {
+			tail--;
+		}
+		Q[++tail] = i;
+		debug(i, dp[i]);
+	}
+	cout << dp[n] << endl;
+}
