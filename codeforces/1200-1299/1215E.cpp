7420771b68f23a85901e14c0b5a3f69daded92d2
goodstudy <goodstudyQAQ@163.com>
2021-07-28 09:22:12 +0800

    [2200] CF1215E 状压dp 逆序对思想

---
+++ codeforces/1200-1299/1215E.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector< int > a(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	vector< int > cnt(21, 0);
+	vector< vector< long long > > sum(21, vector< long long >(21, 0));
+
+	for (int i = 1; i <= n; i++) {
+		cnt[a[i]]++;
+		for (int j = 1; j <= 20; j++)
+			if (j != a[i]) {
+				sum[a[i]][j] += cnt[j];
+			}
+	}
+
+	int LIMIT = 1 << 20;
+	vector< long long > dp(LIMIT, 1LL * n * n + 10);
+	dp[0] = 0;
+	for (int i = 1; i < LIMIT; i++) {
+		for (int j = 0; j < 20; j++) {
+			if ((i >> j) & 1) {
+				long long tmp = 0;
+				for (int k = 0; k < 20; k++) {
+					if ((i >> k) & 1) {
+						tmp += sum[j + 1][k + 1];
+					}
+				}
+
+				dp[i] = min(dp[i], dp[i - (1 << j)] + tmp);
+			}
+		}
+	}
+	cout << dp[LIMIT - 1] << endl;
+	return 0;
+}
