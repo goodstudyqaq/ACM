670300dd1c220f41ca9fb71f7aae9ea7148d8d0f
goodstudy <goodstudyQAQ@163.com>
2021-08-03 08:47:28 +0800

    [2200] CF908D 期望dp 推式子 无穷级数之和

---
+++ codeforces/900-999/908D.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+const int mod = 1e9 + 7;
+long long quick(long long a, long long b) {
+	long long res = 1;
+	while (b) {
+		if (b % 2) res = res * a % mod;
+		a = a * a % mod;
+		b /= 2;
+	}
+	return res;
+}
+long long inv_pa, inv_pb, inv_pab;
+int k, pa, pb;
+
+vector< vector< long long > > dp;
+long long dfs(int i, int j) {
+	if (i + j >= k) {
+		return ((1LL * pa * inv_pb % mod + i) % mod + j) % mod;
+	}
+	if (dp[i][j] != -1) return dp[i][j];
+
+	long long res = (dfs(i + 1, j) * pa % mod * inv_pab % mod + dfs(i, j + i) * pb % mod * inv_pab % mod) % mod;
+	return dp[i][j] = res;
+}
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> k >> pa >> pb;
+	inv_pa = quick(pa, mod - 2);
+	inv_pb = quick(pb, mod - 2);
+	inv_pab = quick(pa + pb, mod - 2);
+	dp.resize(k, vector< long long >(k, -1));
+	cout << dfs(1, 0) << endl;
+	return 0;
+}
