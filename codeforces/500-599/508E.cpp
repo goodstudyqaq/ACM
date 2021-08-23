b5b340d3a8c5b94cfaed94c6b075df77a6318a3f
goodstudy <goodstudyQAQ@163.com>
2021-08-23 09:47:22 +0800

    [2200] CF508E 栈 贪心

---
+++ codeforces/500-599/508E.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+typedef pair< int, int > pii;
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector< int > stk(n + 1);
+	vector< pii > range(n + 1);
+	vector< int > pos(n + 1);
+	int scnt = 0;
+	string ans = "";
+
+	for (int i = 1; i <= n; i++) {
+		cin >> range[i].first >> range[i].second;
+		debug(i);
+		stk[scnt++] = i;
+		ans += "(";
+		pos[i] = ans.size() - 1;
+		while (scnt > 0) {
+			int idx = stk[scnt - 1];
+			int the_pos = pos[idx];
+			int val = ans.size() - the_pos;
+			if (val > range[idx].second) {
+				cout << "IMPOSSIBLE" << endl;
+				return 0;
+			}
+			if (val >= range[idx].first) {
+				ans += ')';
+				scnt--;
+			} else {
+				break;
+			}
+		}
+	}
+
+	if (scnt) {
+		cout << "IMPOSSIBLE" << endl;
+		return 0;
+	}
+	cout << ans << endl;
+	return 0;
+}
