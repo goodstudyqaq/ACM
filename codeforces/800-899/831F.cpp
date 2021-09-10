c8b80169ccceedc67885fbdfea499d236398f05d
goodstudy <goodstudyQAQ@163.com>
2021-09-10 21:57:14 +0800

    [2300] CF831F 式子推导 分块

---
+++ codeforces/800-899/831F.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int ceil(int a, int b) {
+	return (a - 1) / b + 1;
+}
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	long long k;
+	cin >> n >> k;
+	vector< int > a(n + 1);
+	vector< long long > seg;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		k += a[i];
+		for (int j = 1; j * j <= a[i]; j++) {
+			seg.push_back(j);
+			seg.push_back(ceil(a[i], j));
+		}
+	}
+	seg.push_back(1e12);
+	sort(seg.begin(), seg.end());
+	int scnt = unique(seg.begin(), seg.end()) - seg.begin();
+	long long ans = 0;
+	for (int i = 0; i < scnt - 1; i++) {
+		long long l = seg[i], r = seg[i + 1];
+		long long tmp = 0;
+		for (int j = 1; j <= n; j++) {
+			tmp += ceil(a[j], l);
+		}
+		long long d = k / tmp;
+		if (d >= l && d < r && ans < d) {
+			ans = d;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
