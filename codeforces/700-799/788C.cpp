ea3d34d04c04c3119508b88de8cd5e3f1800acff
goodstudy <goodstudyQAQ@163.com>
2021-09-11 10:34:52 +0800

    [2300] CF788C 转换 bfs 最短路

---
+++ codeforces/700-799/788C.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	unordered_set< int > vis;
+	unordered_map< int, int > dp;
+	queue< int > Q;
+	for (int i = 1; i <= k; i++) {
+		int a;
+		cin >> a;
+		a -= n;
+		if (vis.count(a)) continue;
+		vis.insert(a);
+		dp[a] = 1;
+		Q.push(a);
+	}
+
+	while (!Q.empty()) {
+		int x = Q.front();
+		Q.pop();
+		if (x == 0) {
+			cout << dp[x] << endl;
+			return 0;
+		}
+		for (auto it : vis) {
+			int tmp = x + it;
+			if (tmp < -1000 || tmp > 1000) continue;
+			if (dp.count(tmp)) continue;
+			dp[tmp] = dp[x] + 1;
+			Q.push(tmp);
+		}
+	}
+	cout << -1 << endl;
+}
