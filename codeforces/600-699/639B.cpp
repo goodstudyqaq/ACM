9aa0401f538d934f1a5e8aa7ecf3d0ef5d228bfd
goodstudy <goodstudyQAQ@163.com>
2021-10-23 00:04:43 +0800

    [2300] CF639B 思维 构造

---
+++ codeforces/600-699/639B.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+	return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+	return to_string(( string )s);
+}
+
+string to_string(bool b) {
+	return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+	bool first = true;
+	string res = "{";
+	for (int i = 0; i < static_cast< int >(v.size()); i++) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(v[i]);
+	}
+	res += "}";
+	return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+	string res = "";
+	for (size_t i = 0; i < N; i++) {
+		res += static_cast< char >('0' + v[i]);
+	}
+	return res;
+}
+
+template < typename A >
+string to_string(A v) {
+	bool first = true;
+	string res = "{";
+	for (const auto& x : v) {
+		if (!first) {
+			res += ", ";
+		}
+		first = false;
+		res += to_string(x);
+	}
+	res += "}";
+	return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+	return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+	return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+	cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+	cerr << " " << to_string(H);
+	debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+#ifdef LOCAL
+	freopen("data.in", "r", stdin);
+#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, d, h;
+	cin >> n >> d >> h;
+
+	if (n == 2) {
+		cout << 1 << ' ' << 2 << endl;
+		return 0;
+	}
+
+	if (d < 2) {
+		cout << -1 << endl;
+		return 0;
+	}
+	int nodes = d + 1;
+	if (h > d || h < nodes / 2) {
+		cout << -1 << endl;
+		return 0;
+	}
+
+	int rt = nodes - h;
+
+	for (int i = 1; i < nodes; i++) {
+		int u = i, v = i + 1;
+		if (u == 1) {
+			u = rt;
+		} else if (u == rt) {
+			u = 1;
+		}
+		if (v == 1) {
+			v = rt;
+		} else if (v == rt) {
+			v = 1;
+		}
+		cout << u << ' ' << v << '\n';
+	}
+	int chose;
+	if (rt == 1) {
+		chose = 2;
+	} else {
+		chose = 1;
+	}
+
+	for (int i = nodes + 1; i <= n; i++) {
+		cout << chose << ' ' << i << '\n';
+	}
+	return 0;
+}
