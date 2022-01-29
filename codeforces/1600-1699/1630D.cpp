ce953cdec3b120c5cb870df204c6424237357200
shen <cguo@wish.com>
2022-01-29 17:15:38 +0800

    CF1630D 思维

---
+++ codeforces/1600-1699/1630D.cpp
@@ -0,0 +1,167 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+template < typename A, typename B >
+string to_string(pair< A, B > p);
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p);
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string)s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector< bool > v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast< int >(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template < size_t N >
+string to_string(bitset< N > v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast< char >('0' + v[i]);
+    }
+    return res;
+}
+
+template < typename A >
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template < typename A, typename B >
+string to_string(pair< A, B > p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template < typename A, typename B, typename C >
+string to_string(tuple< A, B, C > p) {
+    return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ")";
+}
+
+template < typename A, typename B, typename C, typename D >
+string to_string(tuple< A, B, C, D > p) {
+    return "(" + to_string(get< 0 >(p)) + ", " + to_string(get< 1 >(p)) + ", " + to_string(get< 2 >(p)) + ", " + to_string(get< 3 >(p)) + ")";
+}
+
+void debug_out() {
+    cerr << endl;
+}
+
+template < typename Head, typename... Tail >
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+#else
+#endif
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        long long sum = 0;
+        vector< int > a, b;
+        a.resize(n + 1);
+        b.resize(n + 1);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            sum += abs(a[i]);
+        }
+        for (int i = 1; i <= m; i++) {
+            cin >> b[i];
+        }
+        int g = b[1];
+        for (int i = 2; i <= m; i++) {
+            g = gcd(g, b[i]);
+        }
+        if (g == 1) {
+            cout << sum << endl;
+            continue;
+        }
+        debug(g, sum);
+
+        vector< int > num(g);
+        vector< int > mi(g, 1e9 + 1);
+        for (int i = 0; i < n; i++) {
+            if (a[i] < 0) {
+                num[i % g] ^= 1;
+            }
+            mi[i % g] = min(mi[i % g], abs(a[i]));
+        }
+        debug(num);
+        long long tmp1 = 0, tmp2 = 0;
+        for (int i = 0; i < g; i++) {
+            if (num[i] == 0) {
+                tmp1 += mi[i];
+            } else {
+                tmp2 += mi[i];
+            }
+        }
+        debug(tmp1, tmp2);
+        tmp1 = min(tmp1, tmp2);
+        cout << sum - tmp1 * 2 << endl;
+    }
+    return 0;
+}
