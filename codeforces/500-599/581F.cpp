9ace28d726e68bec85e1ab00275c07a61224a2dc
goodstudy <goodstudyQAQ@163.com>
2022-02-22 23:04:36 +0800

    [2400] CF581F 树形dp

---
+++ codeforces/500-599/581F.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string)s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() {
+    cerr << endl;
+}
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+const int inf = 0x3f3f3f3f;
+vector<vector<int>> V;
+vector<vector<vector<int>>> dp;
+vector<int> sz;
+int leaf_cnt;
+// dp[0/1][j][k] -> j 选 0 / 1 的子树下 0 的个数为 k 的最小个数
+void dfs(int u, int pre) {
+    if (V[u].size() == 1) {
+        dp[0][u][0] = inf;
+        dp[0][u][1] = 0;
+        dp[1][u][0] = 0;
+        dp[1][u][1] = inf;
+        sz[u] = 1;
+        return;
+    }
+    sz[u] = 0;
+    for (auto v : V[u]) {
+        if (v == pre) continue;
+        dfs(v, u);
+        vector<vector<int>> tmp(2, vector<int>(sz[u] + sz[v] + 1, inf));
+        for (int i = 0; i <= sz[u]; i++) {
+            for (int j = 0; j <= sz[v]; j++) {
+                tmp[0][i + j] = min({tmp[0][i + j], dp[0][u][i] + dp[0][v][j], dp[0][u][i] + dp[1][v][j] + 1});
+                tmp[1][i + j] = min({tmp[1][i + j], dp[1][u][i] + dp[0][v][j] + 1, dp[1][u][i] + dp[1][v][j]});
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j <= sz[u] + sz[v]; j++) {
+                dp[i][u][j] = tmp[i][j];
+            }
+        }
+        sz[u] += sz[v];
+    }
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    V.resize(n + 1);
+    sz.resize(n + 1);
+    dp.resize(2, vector<vector<int>>(n + 1, vector<int>(n + 1)));
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        V[u].push_back(v);
+        V[v].push_back(u);
+    }
+    if (n == 2) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    int rt;
+    for (int i = 1; i <= n; i++) {
+        if (V[i].size() > 1) {
+            rt = i;
+        } else {
+            leaf_cnt++;
+        }
+    }
+    dfs(rt, 0);
+    cout << min(dp[0][rt][leaf_cnt / 2], dp[1][rt][leaf_cnt / 2]) << endl;
+    return 0;
+}
