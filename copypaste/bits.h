42c088c1d01f79922fea7ba2c6f821fef9fe7134
goodstudy <goodstudyQAQ@163.com>
2022-02-22 22:13:18 +0800

    add bits

--- copypaste/bits.h
+++ copypaste/bits.h
@@ -12,7 +12,7 @@ struct LoopCollection {
         所以 lowbit(x) = x & -x
     */
 
-    void loopSet(vector< int > a) {
+    void loopSet(vector<int> a) {
         // 遍历 {0, 1, ..., n - 1} 的所有子集
         int n = a.size();
         int limit = 1 << n;
@@ -41,14 +41,32 @@ struct LoopCollection {
         }
 
         // 真子集
-        for (int sub = (status - 1) & status, ok = true; ok; ok = (sub != status)) {
+        for (int sub = (status - 1) & status; sub != status; sub = (sub - 1) & status) {
             // do(sub)
-            sub = (sub - 1) & status;
         }
 
         // 非空真子集
         for (int sub = (status - 1) & status; sub; sub = (sub - 1) & status) {
             // do(sub)
+        }
+    }
+    void loopMultiSubset(vector<int> a) {
+        // 求多个集合(状压)的所有非空子集组成的集合
+        // https://ac.nowcoder.com/acm/contest/7607/B
+        vector<bool> have(1e6 + 1, 0);
+        function<void(int v)> f = [&](int v) {
+            if (have[v]) return;
+            have[v] = true;
+            for (int w = v; w > 0; w &= w - 1) {
+                f(v ^ (w & -w));
+            }
+        };
+    }
+
+    void loopSuperset(int n, int status) {
+        // 枚举 status 的全部超集(父集) ss
+        for (int ss = status; ss < (1 << n); ss = (ss + 1) | status) {
+            // do(ss)...
         }
     }
 };
