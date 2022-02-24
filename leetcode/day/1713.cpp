01db4b132f4d0cae2af8e0ad20fcb14d732e19cb
goodstudy <goodstudyQAQ@163.com>
2022-02-25 00:32:15 +0800

    [leetcode] 1713 dp LIS

---
+++ leetcode/day/1713.cpp
@@ -0,0 +1,28 @@
+// copy_start_symbol=hello copy_end_symbol=world
+#include "../header.h"
+
+// hello
+class Solution {
+   public:
+    int minOperations(vector<int>& target, vector<int>& arr) {
+        map<int, int> M;
+        for (int i = 0; i < target.size(); i++) {
+            M[target[i]] = i;
+        }
+
+        int m = arr.size();
+        vector<int> res;
+        for (int i = 0; i < m; i++) {
+            if (!M.count(arr[i])) continue;
+            auto it = lower_bound(res.begin(), res.end(), M[arr[i]]);
+            if (it == res.end()) {
+                res.push_back(M[arr[i]]);
+            } else {
+                *it = M[arr[i]];
+            }
+        }
+        return m - res.size();
+    }
+};
+
+// world
