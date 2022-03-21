950ab9006c0d453b5ba3969b385cfc0ec04d17bf
goodstudy <goodstudyQAQ@163.com>
2022-03-21 21:15:33 +0800

    [2800] CF1654F 倍增排序的思想

---
+++ codeforces/1600-1699/1654F.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string)s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() {
+    cerr << endl;
+}
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+template <class T>
+auto vect(const T& v, int n) { return vector<T>(n, v); }
+template <class T, class... D>
+auto vect(const T& v, int n, D... m) {
+    return vector<decltype(vect(v, m...))>(n, vect(v, m...));
+}
+
+using pii = pair<int, int>;
+using piii = pair<int, pii>;
+template <typename T>
+static constexpr T inf = numeric_limits<T>::max() / 2;
+mt19937_64 mrand(random_device{}());
+long long rnd(long long x) { return mrand() % x; }
+int lg2(long long x) { return sizeof(long long) * 8 - 1 - __builtin_clzll(x); }
+
+struct fast_ios {
+    fast_ios() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    };
+} fast_ios_;
+
+int main() {
+#ifdef LOCAL
+    freopen("./data.in", "r", stdin);
+#endif
+    int n;
+    string s;
+    cin >> n >> s;
+    int LIMIT = 1 << n;
+
+    vector<int> a(LIMIT);
+    iota(a.begin(), a.end(), 0);
+    sort(a.begin(), a.end(), [&](int x, int y) { return s[x] < s[y]; });
+
+    vector<int> b(LIMIT, 0);
+    for (int i = 1; i < LIMIT; i++) {
+        b[a[i]] = b[a[i - 1]] + (s[a[i]] != s[a[i - 1]]);
+    }
+    debug(a);
+    debug(b);
+
+    for (int i = 0; i < n; i++) {
+        int s = 1 << i;
+        auto cmp = [&](int x, int y) {
+            if (b[x] != b[y]) {
+                return b[x] < b[y];
+            }
+            return b[x ^ s] < b[y ^ s];
+        };
+        sort(a.begin(), a.end(), cmp);
+
+        vector<int> tmp(LIMIT);
+        for (int j = 1; j < LIMIT; j++) {
+            tmp[a[j]] = tmp[a[j - 1]] + cmp(a[j - 1], a[j]);
+        }
+        b = tmp;
+        debug(i, a, b);
+    }
+    int x = a[0];
+    debug(x);
+
+    for (int i = 0; i < LIMIT; i++) {
+        cout << s[i ^ x];
+    }
+    cout << endl;
+    return 0;
+}
