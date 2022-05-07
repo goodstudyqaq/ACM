12d13d054f46319640dcac51123280e3bc7dbdf1
goodstudy <goodstudyQAQ@163.com>
2022-05-07 21:31:31 +0800

    [copypaste] add string stl

--- copypaste/string/stl.h
+++ copypaste/string/stl.h
@@ -11,4 +11,34 @@ void StringSTL() {
     int add = s.find('+', 0);  // 从 pos 开始查找，返回第一个匹配的下标
 }
 
+vector<string> split(string s, string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
+/*
+%02d 2位数字，不满补 0
+*/
+template <typename... Args>
+std::string string_format(const std::string& format, Args... args) {
+    int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1;  // Extra space for '\0'
+    if (size_s <= 0) {
+        throw std::runtime_error("Error during formatting.");
+    }
+    auto size = static_cast<size_t>(size_s);
+    std::unique_ptr<char[]> buf(new char[size]);
+    std::snprintf(buf.get(), size, format.c_str(), args...);
+    return std::string(buf.get(), buf.get() + size - 1);  // We don't want the '\0' inside
+}
+
 }  // namespace STL
