cd8de00427c6d4023faaabfbc134c1a7ad255d10
goodstudy <goodstudyQAQ@163.com>
2022-05-26 23:20:16 +0800

    [cp] add bit

---
+++ copypaste/dataStruct/bit.h
@@ -0,0 +1,22 @@
+template <typename T>
+struct Fenwick {
+#define lowbit(x) x & -x
+    const int n;
+    vector<T> a;
+    Fenwick(int n) : n(n), a(n + 1) {}
+    void add(int x, T v) {
+        while (x <= n) {
+            a[x] += v;
+            x += lowbit(x);
+        }
+    }
+
+    T query(int x) {
+        T res = 0;
+        while (x) {
+            res += a[x];
+            x -= lowbit(x);
+        }
+        return res;
+    }
+};
