42fe955824795d086cee7bb0b3f8463808902e2b
goodstudy <goodstudyQAQ@163.com>
2022-06-14 11:39:03 +0800

    [cp] matrix 增加行列式计算

--- copypaste/math/matrix.h
+++ copypaste/math/matrix.h
@@ -2,6 +2,7 @@
 using namespace std;
 const int mod = 1e9 + 7;
 const int maxn = 105;
+typedef long long ll;
 struct Matrix {
     int n, m;
     vector<vector<long long>> mat;
@@ -43,6 +44,37 @@ struct Matrix {
             tmp = tmp * tmp;
         }
         return res;
+    }
+
+    //行列式辗转相除法
+    /*
+    https://atcoder.jp/contests/abc253/tasks/abc253_h
+    矩阵树定理：维护一个度数矩阵D 和一个邻接矩阵A，Kirchhoff矩阵= D - A，然后把最后一行和最后一列删掉，算行列式，就是树的个数。
+    */
+    ll det(ll mod) {
+        ll ret = 1;
+        for (int i = 0; i < n; i++) {
+            if (mat[i][i] < 0) {
+                ret = -ret;
+                for (int k = i; k < n; k++) mat[i][k] = -mat[i][k];
+            }
+            for (int j = i + 1; j < n; j++) {
+                for (int k = i; k < n; k++) mat[i][k] %= mod, mat[j][k] %= mod;
+                while (mat[j][i]) {
+                    if (mat[j][i] < 0) {
+                        ret = -ret;
+                        for (int k = i; k < n; k++) mat[j][k] = -mat[j][k];
+                    }
+                    ll t = mat[i][i] / mat[j][i];
+                    for (int k = i; k < n; k++) mat[i][k] = (mat[i][k] - t * mat[j][k]) % mod;
+                    for (int k = i; k < n; k++) swap(mat[i][k], mat[j][k]);
+                    ret = -ret;
+                }
+            }
+            if (mat[i][i] == 0) return 0;
+            ret = ret * mat[i][i] % mod;
+        }
+        return (ret + mod) % mod;
     }
 };
 
