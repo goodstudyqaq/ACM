0986ac1736070118f85cf49874e319b319bf921a
goodstudy <goodstudyQAQ@163.com>
2022-06-15 23:57:02 +0800

    [cp] set update

--- copypaste/utils/set.h
+++ copypaste/utils/set.h
@@ -1,5 +1,10 @@
 // set 常见使用套路
 // 维护区间
+/* 
+现在模板的逻辑：[l, r], [r+1, r2] => [l, r2], 如果想改成 [l, r], [r, r2] => [l, r2] 可以自己变通
+https://atcoder.jp/contests/abc254/tasks/abc254_g
+https://leetcode.com/problems/count-integers-in-intervals/
+*/
 #include <bits/stdc++.h>
 using namespace std;
 class CountIntervals {
@@ -15,7 +20,7 @@ class CountIntervals {
     }
 
     void add(int left, int right) {
-        auto it = S.upper_bound({left - 1, -1});
+        auto it = S.lower_bound({left, -1});  // 目的是想找到第一个包围 [left - 1, right + 1] 的区间。
         if (it != S.begin()) {
             if (prev(it)->second >= left - 1) {
                 it--;
@@ -36,5 +41,17 @@ class CountIntervals {
 
     int count() {
         return ans;
+    }
+
+    pii get_interval(int idx) {
+        auto it = S.lower_bound({idx + 1, -1});  // 写法很关键，找最后一个包围 idx 的区间可以这么写
+        if (it == S.begin()) {
+            return {-1, -1};
+        }
+        it--;
+        if (it->second < idx) {
+            return {-1, -1};
+        }
+        return *it;
     }
 };
