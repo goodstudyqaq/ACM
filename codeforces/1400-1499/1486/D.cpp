5a5deed466784ddb5985289c4ec22a1890846f54
goodstudy <goodstudyQAQ@163.com>
2022-08-23 23:09:36 +0800

    [cf] 1486D

---
+++ codeforces/1400-1499/1486/D.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p);
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p);
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string)s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+string to_string(vector<bool> v) {
+    bool first = true;
+    string res = "{";
+    for (int i = 0; i < static_cast<int>(v.size()); i++) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(v[i]);
+    }
+    res += "}";
+    return res;
+}
+
+template <size_t N>
+string to_string(bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+
+template <typename A, typename B, typename C, typename D>
+string to_string(tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+void debug_out() {
+    cerr << endl;
+}
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+#ifdef LOCAL
+    freopen("data.in", "r", stdin);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n + 1);
+    vector<int> x(n);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        x[i - 1] = a[i];
+    }
+    sort(x.begin(), x.end());
+    int x_num = unique(x.begin(), x.end()) - x.begin();
+    vector<int> new_a(n + 1);
+    vector<int> sum(n + 1);
+    vector<int> pre_mi(n + 1);
+    pre_mi[0] = 0;
+    auto check = [&](int val) -> bool {
+        for (int i = 1; i <= n; i++) {
+            if (a[i] < val)
+                new_a[i] = -1;
+            else if (a[i] >= val)
+                new_a[i] = 1;
+            sum[i] = sum[i - 1] + new_a[i];
+            pre_mi[i] = min(pre_mi[i - 1], sum[i]);
+        }
+        for (int i = 1; i <= n; i++) {
+            int l = i - k;
+            if (l < 0) continue;
+            int sum1 = sum[i];
+            int sum2 = pre_mi[l];
+            int tmp = sum1 - sum2;
+            if (tmp > 0) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    int l = 0, r = x_num - 1;
+    int ans = -1;
+    while (l <= r) {
+        int m = l + r >> 1;
+        if (check(x[m])) {
+            ans = x[m];
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
