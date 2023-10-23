0509a1d8fe4e680e475d6172dc12ef178d0ddb38
shen <goodstudyQAQ@163.com>
2023-10-23 14:29:23 +0800

    [cf] 1443 C

---
+++ codeforces/1400-1499/1443/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "copypaste/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define endl '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+struct fast_ios {
+    fast_ios() {
+        cin.tie(nullptr);
+        ios::sync_with_stdio(false);
+        cout << fixed << setprecision(10);
+    };
+} fast_ios_;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    long long ans = accumulate(b.begin(), b.end(), 0LL);
+    vector<int> f(n);
+    iota(f.begin(), f.end(), 0);
+    sort(f.begin(), f.end(), [&](int x, int y) {
+        return a[x] < a[y];
+    });
+    long long right = ans;
+
+    for (int i = 0; i < n; i++) {
+        int idx = f[i];
+        right -= b[idx];
+        ans = min(ans, max(1LL * a[idx], right));
+    }
+    cout << ans << endl;
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("./data.in", "r", stdin);
+#endif
+
+    int T;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
